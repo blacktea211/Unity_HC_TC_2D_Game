234d703cab14e7eb5c2cc057cc7f73741ab9d881
blacktea211 <70305824+blacktea211@users.noreply.github.com>
2020-12-03 21:34:10 +0800

    認識C#四大屬性

---
+++ 2DGame/Assets/Script/Car.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    //單行註解
+    // 備註=數值
+    // 尺寸3m
+    // 重量
+    // 顏色
+
+    int size;3
+    float weight;300.7
+    string brand; toyota
+    bool skyWindow; true 
+   
+
+}
