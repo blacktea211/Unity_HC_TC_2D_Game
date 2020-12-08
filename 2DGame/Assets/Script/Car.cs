491c8775a094cfaf65f1304a6dd86661947a845c
blacktea211 <70305824+blacktea211@users.noreply.github.com>
2020-12-08 21:51:41 +0800

    常用屬性

--- 2DGame/Assets/Script/Car.cs
+++ 2DGame/Assets/Script/Car.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
@@ -8,10 +8,60 @@ public class Car : MonoBehaviour
     // 重量
     // 顏色
 
-    int size;3
-    float weight;300.7
-    string brand; toyota
-    bool skyWindow; true
-   
+    //指定值
+    //私人 private 不會顯示在屬性面板
+    //公開 public  會顯示在屬性面板
 
+    //欄位屬性
+    //標題Header
+    //提示Tooltip
+    [Header("車子尺寸"), Range(0, 500)]
+    public int size = 3;
+    [Header("車子重量"), Tooltip("車子重量單位:噸")]
+    public float weight = 300.7f;
+    public string brand = "toyota";
+    public bool skyWindow = true;
+
+    //其他常用類型
+    //顏色 Color
+    public Color colorA = Color.red;//內建顏色
+    public Color colorB = new Color(0, 0, 1);//內建顏色 (R,G,B)
+    public Color colorC = new Color(0,0, 1, 0.5f);//內建顏色 (R,G,B,A透明度)
+
+    //向量 Vector 2-4 (2維~4維)
+    public Vector2 v2A = Vector2.zero; //2維
+    public Vector2 v2B = Vector2.one;
+    public Vector2 v2C = new Vector2 (0, 1);
+
+    public Vector3 v3A = new Vector3(0, 1, 1); //3維
+    public Vector4 v4A = new Vector4(0, 1, 1, 1); //4維
+    
+    //音效
+    public AudioClip soundExplosion;
+
+    //圖片
+    public Sprite spriteA;
+
+    //遊戲物件與預置物 GameObjet
+    public GameObject gameObject1;
+
+    //元件:屬性面板上的"粗體字"
+    public Transform tra;
+
+    public Sprite Render;
+
+
+    //事件-開始事件 (訊息)
+    private void Start()
+    {
+        print("helloworld");
+
+     //取得欄位(抓出"屬性面板上"的資料)
+        print(size);
+        print(weight);
+
+     //設定欄位(修改資料)
+        weight = 100.5f;
+        skyWindow = false;
+    }
 }
---
+++ 2DGame/Assets/Script/TetrisManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TetrisManager : MonoBehaviour
+{
+    public float falltime = 1.5f;
+    [Header("掉落時間"), Range(0.1f, 3)]
+
+    public int score;
+    [Header("目前分數")]
+
+    public int hightscore;
+    [Header("最高分數")]
+
+    public int level = 1;
+    [Header("等級")]
+    
+    public GameObject gmaeover;
+    [Header("結束畫面")]
+
+    public AudioClip fallsound;
+    [Header("方塊掉落音效")]
+
+    public AudioClip movesound;
+    [Header("方塊移動與旋轉音效")]
+
+    public AudioClip cleansound;
+    [Header("方塊消除音效")]
+
+    public AudioClip endsound;
+    [Header("方塊消除音效")]
+
+}
