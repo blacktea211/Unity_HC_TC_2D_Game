de60449bbfb2ebe70ef97a18b467554c5fc9e542
blacktea211 <70305824+blacktea211@users.noreply.github.com>
2020-12-17 21:51:40 +0800

    開始遊戲並讓方塊移至遊戲區

---
+++ 2DGame/Assets/Script/APINoStatic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class APINoStatic : MonoBehaviour
+{
+    public Transform traA;
+    public Transform traB;
+    public GameObject mygameObject ;
+    public Transform mytra;
+
+    private void Start()
+    {
+        print("物件的座標" + traA.position);
+
+        traB.position = new Vector3(2, 4, 6);
+
+        print("物件圖曾為:" + mygameObject.layer);
+        mygameObject.layer = 4;
+
+        
+
+    }
+
+ 
+    void Update()
+    {
+        mytra.Rotate(1, 2, 3);
+
+    }
+}
--- 2DGame/Assets/Script/TetrisManager.cs
+++ 2DGame/Assets/Script/TetrisManager.cs
@@ -29,30 +29,85 @@ public class TetrisManager : MonoBehaviour
     [Header("方塊消除音效")]
     public AudioClip endsound;
 
+    [Header("下一個俄羅斯方塊區域")]
+    public Transform traNextArea;
+
+    [Header("畫布")]
+    public Transform  traCanvas;
+
+
+    /// <summary>
+    /// 下一個俄羅斯方塊編號
+    /// </summary>
+    public int indexnext;
+
+    private void Start()
+    {
+        SpawnTetris();
+
+
+    }
+
     //方法
-    private void newTetris()
+    /// <summary>
+    /// 生成俄羅斯方塊
+    /// 隨機顯示下一個方塊(隨機顯示0~10的方塊)
+    /// </summary>
+    private void SpawnTetris()
     {
+        //下一個編號 = 隨機的範圍(最小,最大)
+        //Random .Range不會顯示最大值 , Random .Range (0,11) => 隨機顯示0~10的方塊
+        indexnext = Random.Range(0, 11);
+
+        //下一個俄羅斯方塊區域 . 取得子物件(子物件編號) . 轉為遊戲物件 . 啟動設定(顯示)
+        traNextArea.GetChild(indexnext).gameObject.SetActive(true);
 
     }
 
-    public  void addscore()
+    /// <summary>
+    /// 開始遊戲
+    /// 1.生成俄羅斯方塊放在正確位置
+    /// 2.上一次俄羅斯方塊隱藏
+    /// 3.隨機取得下一個方塊
+    /// </summary>
+    public void StartGame()
     {
+        //1.生成俄羅斯方塊要放的位置
+        //保存上一次的俄羅斯方塊
+        GameObject tetris = traNextArea.GetChild(indexnext).gameObject;
 
+        //目前俄羅斯方塊=生成物件(物件，父物件)
+        GameObject current = Instantiate(tetris, traCanvas);
+
+        //GetComponent<任何元件>()
+        //<T>泛型 - 指所有類型
+        //目前俄羅斯方塊 . 取得元件<介面變形>() . 座標 .二維向量
+        current.GetComponent<RectTransform>().anchoredPosition = new Vector2(40, 400);
+
+        //2.上一次俄羅斯方塊隱藏
+        tetris.SetActive(false);
+        //3.隨機取得下一個
+        SpawnTetris();
+    }
+   
+    public  void Addscore()
+    {
+         
     }
 
-    private void gametime()
+    private void Gametime()
     {
     }
 
-    private void end()
+    private void End()
     {
     }
 
-    public void again()
+    public void Again()
     {
     }
 
-    public void exit()
+    public void QuitGame()
     {
     }
 
